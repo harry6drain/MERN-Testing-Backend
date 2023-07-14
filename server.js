c1db401729eb9ccea941c1e32dd5b39679568571
Harry Liu <harryliu23333@gmail.com>
2023-07-14 18:46:27 +0800

    Testing port

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ connection.once('open',() => {
     console.log("MongoDB connection established successfully!")
 })
 
+app.get("/",(req,res) => {
+  res.send("Hello!!!")
+})
 app.use("/workouts",workOutRoutes)
 
 const port = process.env.PORT || 4000

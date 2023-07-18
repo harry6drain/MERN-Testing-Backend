e936dd3500df4fdd1a1d4b7a326a0d88513216b3
Harry Liu <harryliu23333@gmail.com>
2023-07-18 21:41:48 +0800

    Changed back to cor()

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
 

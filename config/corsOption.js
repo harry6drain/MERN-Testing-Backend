d03f3298705aba11f0377eb40e4ff9deb3b6de23
Harry Liu <harryliu23333@gmail.com>
2023-07-18 21:27:36 +0800

    Debugging CORS

--- config/corsOption.js
+++ config/corsOption.js
@@ -1,9 +1,11 @@
 var whitelist = ['http://13.59.254.124']
 var corsOptions = {
   origin: function (origin, callback) {
+    console.log('Origin:', origin);
     if (whitelist.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
+      console.log("Not allowed by CORS")
       callback(new Error('Not allowed by CORS'))
     }
   }

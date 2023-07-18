650b8bbef2941a67052de66b84c6f18b96433d54
Harry Liu <harryliu23333@gmail.com>
2023-07-18 17:30:39 +0800

    Changed server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const workOutRoutes = require("./routes/workouts")
 const corsOptions = require("./config/corsOption")
+const path = require("path")
 
 require("dotenv").config();
 
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(cors())
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..",'frontend','build')));
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true});

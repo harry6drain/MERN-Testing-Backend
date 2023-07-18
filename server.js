280b13e7ac3691e2a7d5ada24f9f36e4b69cac0d
Harry Liu <harryliu23333@gmail.com>
2023-07-18 21:10:31 +0800

    Switched back

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
 

8856e2b289166b29d18b5d4657a6be38289fbe94
Harry Liu <harryliu23333@gmail.com>
2023-07-18 18:32:42 +0800

    Changed Server.js

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const app = express();
 
 app.use(cors())
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..",'frontend','build')));
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true});
@@ -20,9 +23,6 @@ connection.once('open',() => {
     console.log("MongoDB connection established successfully!")
 })
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..','frontend','build','index.html'));
-});
 
 app.use("/workouts",workOutRoutes)
 

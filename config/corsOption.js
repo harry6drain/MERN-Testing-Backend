64ac3b448db78d59625455ea0f805e590b889fce
Harry Liu <harryliu23333@gmail.com>
2023-07-20 16:24:06 +0800

    CORS

--- config/corsOption.js
+++ config/corsOption.js
@@ -1,8 +1,8 @@
-var whitelist = ['http://13.59.254.124:4000']
+var whitelist = ['http://13.59.254.124']
 var corsOptions = {
   origin: function (origin, callback) {
     console.log('Origin:', origin);
-    if (whitelist.indexOf(origin) !== -1) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true)
     } else {
       console.log("Not allowed by CORS")

5404711508a8117f96d893e893f105e8b6c6023d
Harry Liu <harryliu23333@gmail.com>
2023-07-20 16:04:14 +0800

    CORS

--- config/corsOption.js
+++ config/corsOption.js
@@ -1,4 +1,4 @@
-var whitelist = ['13.59.254.124','http://localhost:3000']
+var whitelist = ['http://13.59.254.124']
 var corsOptions = {
   origin: function (origin, callback) {
     console.log('Origin:', origin);

c2f6d2df9236f592c2c9ad79dc4126ed55004671
Harry Liu <harryliu23333@gmail.com>
2023-07-21 20:01:42 +0800

    CORS

--- config/corsOption.js
+++ config/corsOption.js
@@ -1,12 +1,13 @@
-var whitelist = ["https://extrackerdemo.fun:4000", "http://13.59.254.124:4000/"];
+var whitelist = ["https://extrackerdemo.fun", "http://13.59.254.124/"];
 var corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
+    origin: whitelist
+    // origin: function (origin, callback) {
+    //     if (whitelist.indexOf(origin) !== -1) {
+    //         callback(null, true);
+    //     } else {
+    //         callback(new Error("Not allowed by CORS"));
+    //     }
+    // },
 };
 
 module.exports = corsOptions;
